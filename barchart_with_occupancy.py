9497f5494f0a5672f1ac516731475176d63f8322
Cody Yang <xiaozhan.yang@icloud.com>
2022-03-22 17:45:02 +0000

    deleted empty lines on the barchart with occupancy

--- barchart_with_occupancy.py
+++ barchart_with_occupancy.py
@@ -6,8 +6,6 @@ from matplotlib.ticker import (MultipleLocator, FormatStrFormatter, AutoMinorLoc
 import matplotlib.ticker as ticker
 import datetime
 
-
-
 def energy_and_occupancy_barchart_design(df_pivot_working_hours,
                                          day_code_list,
                                          tick_range_e,
@@ -16,7 +14,7 @@ def energy_and_occupancy_barchart_design(df_pivot_working_hours,
                                          path_for_fig = None,
                                          tick_range_o= None,
                                          df_occupancy_cur= None):
-        
+
         fig, ax = plt.subplots(1, 1, figsize=fs)#(8, 3.5) -- Charter house and academy
 
         plt.style.use('seaborn-white')# set ggplot style
@@ -43,38 +41,38 @@ def energy_and_occupancy_barchart_design(df_pivot_working_hours,
 
         hours_labels = {True: "Out Of Hours", False: "In Hours"}
         hours_colors = {True: "w", False: colors_ax_l[0]}
-        
+
         bar_edgecolour = ['k','w']
         bar_fillcolour = ['k','w']
 
 
         # bottom bar legend label
-        ax_l.bar(df_pivot_working_hours.index, df_pivot_working_hours[bot_hours].fillna(0)-2/1000, 
-                 width=0.5, lw=1.2, color=hours_colors[bot_hours], 
+        ax_l.bar(df_pivot_working_hours.index, df_pivot_working_hours[bot_hours].fillna(0)-2/1000,
+                 width=0.5, lw=1.2, color=hours_colors[bot_hours],
                  edgecolor=bar_edgecolour[0], label=hours_labels[bot_hours])
         # top bar legend label
-        ax_l.bar(df_pivot_working_hours.index, df_pivot_working_hours[top_hours].fillna(0), 
-                 width=0.5, lw=1.2, color=hours_colors[top_hours], 
+        ax_l.bar(df_pivot_working_hours.index, df_pivot_working_hours[top_hours].fillna(0),
+                 width=0.5, lw=1.2, color=hours_colors[top_hours],
                  edgecolor=bar_edgecolour[0], bottom=df_pivot_working_hours[bot_hours].fillna(0)-2/1000, label=hours_labels[top_hours])
         # edge of bar
-        ax_l.bar(df_pivot_working_hours.index, df_pivot_working_hours[top_hours].fillna(0)+df_pivot_working_hours[bot_hours].fillna(0), 
+        ax_l.bar(df_pivot_working_hours.index, df_pivot_working_hours[top_hours].fillna(0)+df_pivot_working_hours[bot_hours].fillna(0),
                  width=0.7, lw=1.3, edgecolor=bar_edgecolour[0], color=bar_fillcolour[1])
 
 
         # bottom bar inner part
-        ax_l.bar(df_pivot_working_hours.index, df_pivot_working_hours[bot_hours].fillna(0)-7/1000, 
+        ax_l.bar(df_pivot_working_hours.index, df_pivot_working_hours[bot_hours].fillna(0)-7/1000,
                  width=0.4, lw=0, color= hours_colors[bot_hours], edgecolor=bar_edgecolour[1])
         # top bar inner part
-        ax_l.bar(df_pivot_working_hours.index, df_pivot_working_hours[top_hours].fillna(0), 
-                 width=0.4, lw=0, color= hours_colors[top_hours], 
+        ax_l.bar(df_pivot_working_hours.index, df_pivot_working_hours[top_hours].fillna(0),
+                 width=0.4, lw=0, color= hours_colors[top_hours],
                  edgecolor=bar_edgecolour[1], bottom=df_pivot_working_hours[bot_hours].fillna(0)-7/1000)
         # black bar for separation
-        ax_l.bar(df_pivot_working_hours.index, df_pivot_working_hours[top_hours]*0, 
-                 width=0.4, lw=1, edgecolor=bar_edgecolour[0], color= bar_fillcolour[0], 
-                 bottom=df_pivot_working_hours[bot_hours].fillna(0)-7/1000) 
+        ax_l.bar(df_pivot_working_hours.index, df_pivot_working_hours[top_hours]*0,
+                 width=0.4, lw=1, edgecolor=bar_edgecolour[0], color= bar_fillcolour[0],
+                 bottom=df_pivot_working_hours[bot_hours].fillna(0)-7/1000)
         # white bar at the bottom
-        ax_l.bar(df_pivot_working_hours.index, df_pivot_working_hours[top_hours]*0+0.002, 
-                 width=0.4, lw=0, edgecolor=bar_edgecolour[1], color= bar_fillcolour[1]) 
+        ax_l.bar(df_pivot_working_hours.index, df_pivot_working_hours[top_hours]*0+0.002,
+                 width=0.4, lw=0, edgecolor=bar_edgecolour[1], color= bar_fillcolour[1])
 
         ax_l.xaxis.set_major_locator(ticker.MultipleLocator(1))
         ax_l.legend(loc='upper left', bbox_to_anchor=(0.025, 0.97))
